4f121d6096992f2520d5754f8fd4b37fa5c2fa41
Ali Guerbuez <8105@htl.rennweg.at>
2023-09-11 15:57:19 +0200

    BritishWeights

--- BritishWeight.py
+++ BritishWeight.py
@@ -2,23 +2,32 @@ class BritishWeight():
 
     lb = 0
 
-    def __init__(self, st, lb):
-        self.lb = lb
+    def __init__(self, st=0, lb=0):
+        self.lb = st*14
+        self.lb += lb
+
 
 
     def __add__(self, other):
         if isinstance(other, BritishWeight):
-            b = BritishWeight(self.lb + other.lb)
-        return b
+            return BritishWeight(lb=self.lb + other.lb)
+
 
 
     def __str__(self):
+        return str (self.stones) + " stones " +  str(self.lb%14) + " Pounds"
+
+    @property
+    def stones(self):
+        return self.lb // 14
 
 
-    def 
+
+b = BritishWeight(1,13)
+print(b.lb)
+a = BritishWeight(2,2)
 
 
-b = BritishWeight(1,2)
-print(b)
+print(a + b)
 
 
