94e22bd03e89ab8583eb6da274dd6c20394aceb2
Ali Guerbuez <8105@htl.rennweg.at>
2023-09-11 15:35:17 +0200

    BritishWeights

--- BritishWeight.py
+++ BritishWeight.py
@@ -1,6 +1,24 @@
 class BritishWeight():
 
-    def __init__(self):
-        
+    lb = 0
+
+    def __init__(self, st, lb):
+        self.lb = lb
+
+
+    def __add__(self, other):
+        if isinstance(other, BritishWeight):
+            b = BritishWeight(self.lb + other.lb)
+        return b
+
+
+    def __str__(self):
+
+
+    def 
+
+
+b = BritishWeight(1,2)
+print(b)
 
 
