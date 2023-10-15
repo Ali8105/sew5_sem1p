da9291039f240a37bb803bf7deefb1d2bf09bd48
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-15 11:37:19 +0200

    create_user

--- u02_python/create_user.py
+++ u02_python/create_user.py
@@ -1,4 +1,7 @@
+import argparse
+import logging
 import unicodedata
+from collections import namedtuple
 
 
 def remove_accent(txt):
@@ -7,3 +10,25 @@ def remove_accent(txt):
                      if not unicodedata.combining(c))
     erg = unicodedata.normalize('NFC', shaved)
     return erg
+
+
+def get_user():
+    User = namedtuple("User", "vnmane nname group u_class login_name")
+
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    output_group = parser.add_mutually_exclusive_group()
+    output_group.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    output_group.add_argument("-q", "--quiet", help="decrease output verbosity", action="store_true")
+    parser.add_argument("file", help="file with the userdata")
+    args = parser.parse_args()
+
+    logger = logging.Logger()
+
+    formatter = logging.Formatter("%(asctime)s; %(levelname)s; %(message)s",
+                                  "%Y-%m-%d %H:%M:%S")
+
+    rotating_file_handler = RotatingFileHandler("res/logs/create_user.log", maxBytes=10000, backupCount=5)
+    rotating_file_handler.setFormatter(formatter)
+    logger.addHandler(rotating_file_handler)
