cc06ee7abb0bfea3ee32c9e4c29e68076f93848e
Ali Guerbuez <8105@htl.rennweg.at>
2023-10-15 11:21:09 +0200

    create_users

--- u02_python/create_user.py
+++ u02_python/create_user.py
@@ -3,4 +3,7 @@ import unicodedata
 
 def remove_accent(txt):
     norm_txt = unicodedata.normalize('NFD',txt)
-    
+    shaved = ''.join(c for c in norm_txt
+                     if not unicodedata.combining(c))
+    erg = unicodedata.normalize('NFC', shaved)
+    return erg
