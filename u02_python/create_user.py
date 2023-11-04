8be79ab8552ae4dd143ee76881548fe407cfd98c
Ali Guerbuez <8105@htl.rennweg.at>
2023-11-04 02:59:44 +0100

    Create_User: all bugs fixed with correct logging

--- u02_python/create_user.py
+++ u02_python/create_user.py
@@ -21,10 +21,10 @@ def generate_scripts():
     """Generiert die Skripts und iteriert uber die Benutzer"""
     with open(r"C:\Users\aligr\Desktop\Schule\5CN\SEW\sew5_sem1p\Ressources\script_user.sh", "w") as file:
         logger.debug("opened file " + file.name)
-        print("set -e", file=file)
+        print("#!/bin/bash \n", file=file)
     with open(r"C:\Users\aligr\Desktop\Schule\5CN\SEW\sew5_sem1p\Ressources\delete_user.sh", "w") as file:
         logger.debug("opened file " + file.name)
-        print("set -x", file=file)
+        print("#!/bin/bash  \n", file=file)
     open(r"C:\Users\aligr\Desktop\Schule\5CN\SEW\sew5_sem1p\Ressources\passwords_users", "w").close()
     users = dict()
     for user in get_user():
@@ -38,7 +38,7 @@ def generate_scripts():
         users[login_name] = login_name
         pw = generate_password()
         user = user._replace(login_name=login_name)
-        create_user_entry(user,pw)
+        create_user_entry(user, pw)
 
 
 def create_user_entry(user, pwd):
@@ -64,7 +64,7 @@ def remove_accent(txt):
 
 def get_user():
     """Generator fur User"""
-    User = namedtuple("User", "vmnam nname group u_class login_name")
+    User = namedtuple("User", "vname nname group u_class login_name")
     for row in ws.iter_rows(min_row=2):
         firstname = remove_accent(str(row[0].value).lower())
         lastname = remove_accent(str(row[1].value).lower())
@@ -77,12 +77,11 @@ def get_user():
 
 def useradd(user, pwd):
     """Useradd in script_user.sh"""
-
     create = f'useradd -d "/home/{user.login_name}" -c "{user.vname + " " + user.nname}" -m ' \
              f'-g {user.group}{"," + user.u_class if user.group == "student" else ""} -s "/bin/bash {user.login_name}" && ' \
              f'echo {user.login_name}:\"{pwd}\" | chpasswd'
 
-    with open(r"C:\Users\aligr\Desktop\Schule\5CN\SEW\sew5_sem1p\Ressources\script_user.sh", "a") as file:
+    with open(r"C:\Users\aligr\Desktop\Schule\5CN\SEW\sew5_sem1p\Ressources\script_user.sh", "a", encoding="utf-8") as file:
         logger.debug("opened file " + file.name)
         print(create, file=file)
         logger.info("wrote useradd into " + file.name)
