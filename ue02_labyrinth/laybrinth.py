070d837a94aa023034ab3c2a29a33cf95b27b7ca
Ali Guerbuez <8105@htl.rennweg.at>
2024-01-20 18:30:53 +0100

    Read_File Methode should be finished now

--- ue02_labyrinth/laybrinth.py
+++ ue02_labyrinth/laybrinth.py
@@ -1,8 +1,18 @@
 __author__ = "Ali Gürbüz"
 
-    def read_labyrinth(file):
-        return 0
+
+def read_labyrinth(file):
+    with open(file,"r") as file_read:
+        counter = 0
+        for line in file_read.readline():
+            laby[counter] = line
+            counter += 1
+    rec_solve(laby)
+
+def rec_solve(laby):
+    return 0
 
 
 if __name__ == '__main__':
+    laby = []
     file = r"l1.txt"
