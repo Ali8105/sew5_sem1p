f5b3c4ae9a3c0621c95e46ae6415fae960d2e103
Ali Guerbuez <8105@htl.rennweg.at>
2024-01-21 21:06:58 +0100

    labyrinth: noch immmter rekursion Fehler

--- ue02_labyrinth/laybrinth.py
+++ ue02_labyrinth/laybrinth.py
@@ -6,25 +6,31 @@ def read_labyrinth(file, posX, posY):
     with open(file,"r") as file_read:
         for line in file_read:
             laby.append(list(line.strip()))
-    path_search(laby,1,1)
+    path_search(laby,posX,posY)
 
 def path_search(laby, posX, posY):
     # Rekursion Break-out
-    if posX < 0 or posY < 0 or posX >= len(laby) or posY >= len(laby[0]):
-        print(laby)
+    if posX < 0 or posY < 0 or posX == len(laby)-1 or posY == len(laby[0])-1:
+        print_path(laby)
         return 0
     else:
         laby[posX][posY] = '*'
-        if laby[posX+1][posY] != '#':
+        if laby[posX+1][posY] == ' ':
             path_search(laby,posX+1,posY)
-        if laby[posX][posY+1] != '#':
+        if laby[posX][posY+1] == ' ':
             path_search(laby,posX,posY+1)
-        if laby[posX-1][posY] != '#':
+        if laby[posX-1][posY] == ' ':
             path_search(laby,posX-1,posY)
-        if laby[posX][posY-1] != '#':
+        if laby[posX][posY-1] == ' ':
             path_search(laby,posX,posY-1)
 
+def print_path(laby):
+    print('---------------------------------')
+    for i in laby:
+        print(i)
+    print('---------------------------------')
 
 if __name__ == '__main__':
     file = r"l1.txt"
     read_labyrinth(file,0,0)
+
