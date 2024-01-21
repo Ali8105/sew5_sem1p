47814761dd30c10cb6100d3934b47feef7e6cf7b
Ali Guerbuez <8105@htl.rennweg.at>
2024-01-21 12:59:30 +0100

    Methoedn name an die Angabe angepasst

--- ue02_labyrinth/laybrinth.py
+++ ue02_labyrinth/laybrinth.py
@@ -1,18 +1,20 @@
 __author__ = "Ali Gürbüz"
 
 
-def read_labyrinth(file):
+def read_labyrinth(file, posX, posY):
     with open(file,"r") as file_read:
         counter = 0
         for line in file_read.readline():
             laby[counter] = line
             counter += 1
-    rec_solve(laby)
+    path_search(laby,posX,posY)
 
-def rec_solve(laby):
+def path_search(laby, posX, posY):
+
     return 0
 
 
 if __name__ == '__main__':
     laby = []
     file = r"l1.txt"
+    read_labyrinth(file,0,0)
