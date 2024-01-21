2ab53d20c7faea1c29e4d0223e3a7b7d2040295a
Ali Guerbuez <8105@htl.rennweg.at>
2024-01-21 20:48:56 +0100

    labyrinth: Statt String jetzt eine Liste damit man es bearbeiten kann

--- ue02_labyrinth/laybrinth.py
+++ ue02_labyrinth/laybrinth.py
@@ -2,18 +2,26 @@ __author__ = "Ali Gürbüz"
 
 
 def read_labyrinth(file, posX, posY):
-    labyrinth = []
+    laby = []
     with open(file,"r") as file_read:
         for line in file_read:
-            labyrinth.append(line.strip())
-    print(labyrinth)
+            laby.append(list(line.strip()))
+    path_search(laby,1,1)
 
 def path_search(laby, posX, posY):
-
-    return 0
+    # Rekursion Break-out
+    if posX < 0 or posY < 0 or posX >= len(laby) or posY >= len(laby[0]):
+        print(laby)
+        return 0
+    else:
+        return 1
+        #laby[posX][posY] = '*'
+        #if laby[posX+1][posY] != '#':
+        #    path_search(laby,posX+1,posY)
+        #if laby[posX][posY+1] != '#':
+        #    path_search(laby,posX,posY+1)
 
 
 if __name__ == '__main__':
-    laby = []
     file = r"l1.txt"
     read_labyrinth(file,0,0)
