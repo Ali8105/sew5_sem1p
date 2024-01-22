7104e92d315b7a7682d98f96e4bbe5a589ddd2c6
Ali Guerbuez <8105@htl.rennweg.at>
2024-01-22 10:17:09 +0100

    Labyrinth: patah search ist fertig

--- ue02_labyrinth/laybrinth.py
+++ ue02_labyrinth/laybrinth.py
@@ -10,22 +10,23 @@ def read_labyrinth(file, posX, posY):
 
 def path_search(laby, posX, posY):
     # Rekursion Break-out
-    if posX < 0 or posY < 0:
-        raise Exception("Ware fast index out of bounds")
-    elif posX >= len(laby)-1 or posY >= len(laby[0])-1:
+       # print_path(laby)
+    if posX < 0 or posY < 0 or posX > len(laby)-1 or posY > len(laby[0])-1:
+        return 0
+    elif laby[posX][posY] == 'A':
         print_path(laby)
     else:
         laby[posX][posY] = '*'
-        if laby[posX+1][posY] == ' ':
+        if laby[posX+1][posY] == ' ' or laby[posX+1][posY] == 'A':
             path_search(laby,posX+1,posY)
-        if laby[posX][posY+1] == ' ':
+        if laby[posX][posY+1] == ' ' or laby[posX][posY+1] == 'A':
             path_search(laby,posX,posY+1)
-        if laby[posX-1][posY] == ' ':
+        if laby[posX-1][posY] == ' ' or laby[posX-1][posY] == 'A':
             path_search(laby,posX-1,posY)
-        if laby[posX][posY-1] == ' ':
+        if laby[posX][posY-1] == ' ' or  laby[posX][posY-1] == 'A':
             path_search(laby,posX,posY-1)
-        print_path(laby)
-        
+        #print_path(laby)
+
 def print_path(laby):
     print('---------------------------------')
     for i in laby:
@@ -33,6 +34,6 @@ def print_path(laby):
     print('---------------------------------')
 
 if __name__ == '__main__':
-    file = r"l1.txt"
-    read_labyrinth(file,0,6)
+    file = r"l3.txt"
+    read_labyrinth(file,0,5)
 
